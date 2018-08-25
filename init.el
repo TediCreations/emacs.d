527d7e90b2ace077f5ef19f0afd84586f2e1171c
tedi <hkanelhs@yahoo.gr>
2018-08-26 00:37:18 +0300

    Removed dual entries

--- init.el
+++ init.el
@@ -19,33 +19,10 @@
 ;;(load "~/.emacs.d/lisp/tests" )
 ;;(require 'tedi-tests)
 
-;;Visiting the configuration
-;;Quickly edit ~/.emacs.d/config.org
-(defun config-visit ()
-  (interactive)
-  (find-file "~/.emacs.d/config.org"))
-(global-set-key (kbd "C-c e") 'config-visit)
-
-;;Reloading the configuration
-;;Simply pressing =Control-c r= will reload this file, very handy.
-;;You can also manually invoke =config-reload=.
-(defun config-reload ()
-  "Reloads ~/.emacs.d/config.org at runtime"
-  (interactive)
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-(global-set-key (kbd "C-c r") 'config-reload)
-
 ;;------------------------------------------------------------------------------
 ;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-
-;;------------------------------------------------------------------------------
-;;Check that all is working
-(global-set-key (kbd "<f5>")
-                (lambda ()
-                  (interactive)
-                  (message "Works!")))
 
 ;;------------------------------------------------------------------------------
 ;;Generated
